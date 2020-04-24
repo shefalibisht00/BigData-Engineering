670da87902c846b84c07323e2a75bdd9fb265f6f
Shefali Bisht <shefalibisht00@gmail.com>
2020-04-24 21:45:25 +0530

    Update TwitterConfig.java

--- kafka-twitter-app/src/main/java/com/github/shefalibisht/kafka/config/TwitterConfig.java
+++ kafka-twitter-app/src/main/java/com/github/shefalibisht/kafka/config/TwitterConfig.java
@@ -2,8 +2,8 @@ package com.github.shefalibisht.kafka.config;
 
 public class TwitterConfig {
 
-    public static final String CONSUMER_KEYS= "H8EVCnz6Hl4l39PefS8SJQ842";
-    public static final String CONSUMER_SECRETS= "xnhE5arCbknAMOykgI97q0EHxd7UDHl3lCk20hi76o47ZhTbdI";
-    public static final String SECRET = "SPzkEZ8P3GadDBJYBCGPf1c9TIvvKxPg9AJiYJFankjXg";
-    public static final String TOKEN = "1333702248-FR1DoRVdaqytzuE1XDCPSeLY3gTDU2kH6aMBuW1";
+    public static final String CONSUMER_KEYS= "";
+    public static final String CONSUMER_SECRETS= "";
+    public static final String SECRET = "";
+    public static final String TOKEN = "";
 }
